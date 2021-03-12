57316b712b441a5f2a0eb2e04e6a2c65c7d0b2ec
Braeden Stewart <braeden.stewart@aggiemail.usu.edu>
2021-03-12 15:29:04 -0700

    added ChatActivity

---
+++ PhoneApp/src/main/java/com/braedenstewartdigitalduckyproject1/phoneapp/ChatActivity.java
@@ -0,0 +1,13 @@
+package com.braedenstewartdigitalduckyproject1.phoneapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
