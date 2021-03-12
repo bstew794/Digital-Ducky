1eb5f04f538c112951cac27042fb1b68b3af1c93
Braeden Stewart <braeden.stewart@aggiemail.usu.edu>
2021-03-12 16:09:08 -0700

    added library view

---
+++ PhoneApp/src/main/java/com/braedenstewartdigitalduckyproject1/phoneapp/LibraryActivity.java
@@ -0,0 +1,14 @@
+package com.braedenstewartdigitalduckyproject1.phoneapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LibraryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_library);
+    }
+}
