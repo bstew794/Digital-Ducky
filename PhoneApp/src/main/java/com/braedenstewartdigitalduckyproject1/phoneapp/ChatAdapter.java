2662c2f8929cfdb44916f016034e403a5c3fc4e4
Braeden Stewart <braeden.stewart@aggiemail.usu.edu>
2021-03-12 14:22:34 -0700

    added ChatAdapter

---
+++ PhoneApp/src/main/java/com/braedenstewartdigitalduckyproject1/phoneapp/ChatAdapter.java
@@ -0,0 +1,34 @@
+package com.braedenstewartdigitalduckyproject1.phoneapp;
+
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.ObservableArrayList;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.braedenstewartdigitalduckyproject1.api.CustomAdapter;
+import com.braedenstewartdigitalduckyproject1.api.Message;
+
+import org.w3c.dom.Text;
+
+public class ChatAdapter extends CustomAdapter<Message> {
+    public ChatAdapter(ObservableArrayList<Message> data){
+        super(data);
+    }
+
+    @Override
+    protected int getLayout() {
+        return R.layout.item_message;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Message message = data.get(position);
+        TextView author = holder.getItemView().findViewById(R.id.author_tv);
+        TextView date = holder.getItemView().findViewById(R.id.date_tv);
+        TextView content = holder.getItemView().findViewById(R.id.content_tv);
+        author.setText(message.getAuthor());
+        date.setText(message.getPublishDate().toString());
+        content.setText(message.getContent());
+    }
+}
